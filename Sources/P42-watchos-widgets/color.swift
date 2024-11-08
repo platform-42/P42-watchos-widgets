ec2f1ebe2adb7a8e9853a0f599f6cf4d0b8059c2
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-11-08 14:06:20 +0100

    Added opacity explicitly

--- Sources/P42-watchos-widgets/color.swift
+++ Sources/P42-watchos-widgets/color.swift
@@ -4,18 +4,15 @@
 //
 //  Created by Diederick de Buck on 24/09/2024.
 //
-
-import Foundation
-
 import Foundation
 import SwiftUI
 
 @available(macOS 10.15, *)
 extension Color {
-    init(hex: Int, opacity: Double = 1.0) {
+    init(hex: Int) {
         let red = Double((hex & 0xff0000) >> 16) / 255.0
         let green = Double((hex & 0xff00) >> 8) / 255.0
         let blue = Double((hex & 0xff) >> 0) / 255.0
-        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: 1.0)
     }
 }
