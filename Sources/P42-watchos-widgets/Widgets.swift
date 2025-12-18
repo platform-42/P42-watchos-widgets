733dfbcb9beaeedcc01428d2657ef6932ef1e89a
Diederick <diederick.de.buck@platform-42.com>
2025-12-18 13:12:28 +0100

    refresh

--- Sources/P42-watchos-widgets/Widgets.swift
+++ Sources/P42-watchos-widgets/Widgets.swift
@@ -135,59 +135,9 @@ public struct NumberAndStatView: View {
     }
 }
 
+
 @available(iOS 13.0, *)
 public struct NumberAndStateView: View {
-    var period: String
-    var primaryValue: String
-    var primaryColor: Color
-    var secondaryValue: String
-    var secondaryColor: Color
-    var widgetState: WidgetState
-
-    public init(period: String,
-                primaryValue: String,
-                primaryColor: Color,
-                secondaryValue: String,
-                secondaryColor: Color,
-                widgetState: WidgetState) {
-        self.period = period
-        self.primaryValue = primaryValue
-        self.primaryColor = primaryColor
-        self.secondaryValue = secondaryValue
-        self.secondaryColor = secondaryColor
-        self.widgetState = widgetState
-    }
-
-    public var body: some View {
-        VStack(spacing: 5) {
-            Text(period.capitalized)
-                .accessibilityLabel("Period: \(period.capitalized)")
-            
-            Divider()
-            Text(primaryValue)
-                .font(.largeTitle)
-                .foregroundColor(primaryColor)
-                .accessibilityLabel("Primary value: \(primaryValue)")
-            HStack {
-                if widgetState != .none {
-                    Image(systemName: Widget.stateFieldImage(widgetState))
-                        .foregroundColor(Widget.stateFieldColor(widgetState))
-                        .padding(5)
-                        .accessibilityLabel("State logic: \(Widget.stateFieldImage(widgetState))")
-                }
-                Text(secondaryValue)
-                    .foregroundColor(secondaryColor)
-                    .padding(5)
-                    .accessibilityLabel("Secondary value: \(secondaryValue)")
-            }
-            Divider()
-        }
-        .padding(.horizontal, 10)
-    }
-}
-
-@available(iOS 13.0, *)
-public struct NumberAndStateView2: View {
     var period: String
     var primaryValue: String
     var primaryColor: Color
